7e3c6b7312b78773a0d5597bb751761d02c4c1d5
Luis Casaos <casaos022@gmail.com>
2024-08-08 14:25:17 -0600

    Login page created V1

---
+++ TT_frontend_web/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import promoImage from '../img/ipn-main.webp';
+
+function Login () {
+
+    return(
+
+            <main className="container mx-auto mt-10 px-4 flex">
+                <section>
+                    <div className="backgound-color: #515254 rounded">
+                        
+                    </div>
+                </section>
+            </main>
+
+    )
+
+}
