20ed145ee6bce450045615da321dc5243d7bd6c6
gustavomontiel155 <110190508+gustavomontiel155@users.noreply.github.com>
2024-08-15 16:43:23 -0600

    Add files via upload

--- TT_frontend_web/src/App.jsx
+++ TT_frontend_web/src/App.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
 import Header from "./components/Header";
 import MainContent from "./pages/Main";
+import Login from "./pages/Login";
 import Footer from "./components/Footer";
+import Registro from './pages/Registro'; 
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <BrowserRouter>
+    <div className="min-h-screen flex flex-col bg-gradient-to-r from-blue-400 to-purple-600">
       <Header />
-      <MainContent className="flex-grow" />
+      <Routes>
+        // Redirigiendo la pagina principar(/) a /welcome con Navigate
+        <Route path='/' element={<Navigate to="/welcome" />} />
+
+        // Asignando que paginas mostrar cuando se visite cierta url
+        <Route path='/welcome' element={<MainContent className="flex-grow" />} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Registro" element={<Registro />} />
+
+      </Routes>
       <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
