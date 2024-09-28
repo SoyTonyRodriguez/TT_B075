c0295ab909e2c61c29a119088ffff773e2f35ff7
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-09-28 17:11:20 -0600

    Fix: Static elements and background when keboard shows

---
+++ TT_frontend_movil/api/accounts.api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+// Initial set-up
+const accountsAPI = axios.create({
+    baseURL: 'http://192.168.1.143:8000/api/v1/'
+})
+
+// register method
+export const createAccount = (account) => accountsAPI.post('register/', account)
+
+// Login method
+export const login = (credentials) => accountsAPI.post('login/', credentials)
+
+// getAccount method
+export const getAccount = (id) => {
+    const token = localStorage.getItem('token');
+    return accountsAPI.get(`account/${id}/`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
---
+++ TT_frontend_movil/api/tasks.api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+// Initial set-up
+const TasksAPI = axios.create({
+    baseURL: 'http://localhost:8000/api/v1/'
+})
+
+// create task method
+// export const createTask = (task) => accountsAPI.post('task/register/', task)
+export const createTask = (task) => {
+    const token = localStorage.getItem('token');
+    return TasksAPI.post('task/register/', task, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    });
+}
+
+
+// getTasks from account method
+export const getTasks = (account_id) => {
+    const token = localStorage.getItem('token');
+    return TasksAPI.get(`tasks/${account_id}/`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
+
+export const updateTaskStatus = (taskId, updatedData) => {
+    const token = localStorage.getItem('token');
+    return TasksAPI.patch(`tasks/${taskId}/edit/`, updatedData, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
+
+export const deleteTask = (taskId) => {
+    const token = localStorage.getItem('token');
+    return TasksAPI.delete(`tasks/${taskId}/delete/`, {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+};
