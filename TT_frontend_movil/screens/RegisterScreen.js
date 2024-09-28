d997a03bf4fb94d2e2ff45fa17a65b85205fd835
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-09-28 17:11:20 -0600

    Fix: Static elements and background when keboard shows

--- TT_frontend_movil/screens/RegisterScreen.js
+++ TT_frontend_movil/screens/RegisterScreen.js
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, ImageBackground, TouchableOpacity, Modal, FlatList } from "react-native";
+import { View, Text, TextInput, ImageBackground, TouchableOpacity, Modal, FlatList, Dimensions, Platform } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import tw from 'twrnc'; 
+import { useForm, Controller } from "react-hook-form";
+import { createAccount } from "../api/accounts.api"; // Import the API
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+const { width, height } = Dimensions.get('window');
 
 const RegisterScreen = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    category: '',
-    employeeNumber: '',
-    password: '',
-    confirmPassword: ''
-  });
   const [categoryModalVisible, setCategoryModalVisible] = useState(false);
+
+  const { control, handleSubmit, reset, setValue, formState: { errors } } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      category: '',
+      employee_number: '',
+      password: '',
+      confirmar_password: ''
+    }
+  });
 
   const categories = [
     "Técnico Docente de Asignatura A",
@@ -37,114 +45,211 @@ const RegisterScreen = () => {
     "Profesor Titular B"
   ];
 
-  const handleInputChange = (name, value) => {
-    setForm({ ...form, [name]: value });
-  };
 
-  const handleRegister = () => {
-    console.log("Datos del formulario:", form);
+  const handleRegister = async (data) => {
+    const password = data.password;
+    const confirmPassword = data.confirmar_password;
+
+    if (password !== confirmPassword) {
+      console.log("Passwords do not match");
+      return;
+    }
+
+    const { confirmar_password, ...accountData } = data;
+
+    try {
+      const response = await createAccount(accountData);
+      console.log("Account created successfully:", response.data);
+    } catch (error) {
+      console.error("Error creating account:", error);
+    }
   };
 
   const selectCategory = (category) => {
-    handleInputChange('category', category);
+    setValue('category', category);
     setCategoryModalVisible(false);
   };
 
+    // Estilo dinámico para la imagen
+  const imageStyle = {
+    width: width,      // Ancho de la ventana
+    height: height,    // Alto de la ventana
+    flex: 1,           // Para que la imagen ocupe todo el contenedor
+  };
+
   return (
-    <ImageBackground 
-      source={require('../assets/images/fondologin.jpg')} 
-      style={tw`flex-1 w-full h-full`}
-      resizeMode="cover"
-    >
-      <View style={tw`flex-1 justify-center px-5`}>
 
-        {/* Header */}
-        <View style={tw`absolute top-10 left-5`}>
-          <View style={tw`flex-row items-center`}>
-            <Text style={tw`text-2xl font-bold text-black`}>Regístrate</Text>
-            <Ionicons name="person-outline" size={30} color="black" style={tw`ml-2`} />
+      <ImageBackground 
+        source={require('../assets/images/fondologin.jpg')} 
+        style={imageStyle}
+        resizeMode="cover"
+      >
+        <KeyboardAwareScrollView 
+          contentContainerStyle={{ flexGrow: 1 }} 
+          resetScrollToCoords={{ x: 0, y: 0 }} 
+          scrollEnabled={true}
+          keyboardShouldPersistTaps="handled" // Permite que los inputs mantengan el foco al hacer scroll
+          >
+          <View style={tw`px-5`}>
+            {/* Cambiamos el header para evitar que se superponga */}
+            <View style={tw`pt-12`}>
+              <View style={tw`flex-row items-center`}>
+                <Text style={tw`text-2xl font-bold text-black`}>Regístrate</Text>
+                <Ionicons name="person-outline" size={30} color="black" style={tw`ml-2`} />
+              </View>
+              <Text style={tw`text-lg text-gray-500 mt-2`}>Introduce los datos solicitados</Text>
+              <View style={tw`h-0.5 bg-black mt-3 mb-5`} />
+            </View>
+
+            {/* Formulario */}
+            <Controller
+              control={control}
+              name="name"
+              rules={{ required: 'El nombre es obligatorio.' }}
+              render={({ field: { onChange, value } }) => (
+                <>
+                  <TextInput
+                    placeholder="Ingresa tu nombre completo"
+                    style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black mb-4`}
+                    placeholderTextColor="rgba(0, 0, 0, 0.8)"
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                  {errors.name && <Text style={tw`text-red-500 mb-4`}>{errors.name.message}</Text>}
+                </>
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: 'El correo electrónico es obligatorio.',
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                  message: 'El formato del correo no es válido.',
+                }
+              }}
+              render={({ field: { onChange, value } }) => (
+                <>
+                  <TextInput
+                    placeholder="Ingresa tu correo electrónico"
+                    style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black mb-4`}
+                    placeholderTextColor="rgba(0, 0, 0, 0.8)"
+                    keyboardType="email-address"
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                  {errors.email && <Text style={tw`text-red-500 mb-4`}>{errors.email.message}</Text>}
+                </>
+              )}
+            />
+
+            <TouchableOpacity onPress={() => setCategoryModalVisible(true)} style={tw`w-full p-4 border border-gray-700 rounded-lg mb-4 flex-row justify-between items-center`}>
+              <Controller
+                control={control}
+                name="category"
+                rules={{ required: 'Selecciona una categoría.' }}
+                render={({ field: { value } }) => (
+                  <>
+                    <Text style={tw`${value ? "text-black" : "text-gray-500"}`}>
+                      {value || "Selecciona tu categoría"}
+                    </Text>
+                    {errors.category && <Text style={tw`text-red-500 mb-4`}>{errors.category.message}</Text>}
+                  </>
+                )}
+              />
+              <Ionicons name="chevron-down" size={24} color="rgba(0, 0, 0, 0.8)" />
+            </TouchableOpacity>
+
+            <Controller
+              control={control}
+              name="employee_number"
+              rules={{ required: 'El número de empleado es obligatorio.' }}
+              render={({ field: { onChange, value } }) => (
+                <>
+                  <TextInput
+                    placeholder="Ingresa tu número de empleado"
+                    style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black mb-4`}
+                    placeholderTextColor="rgba(0, 0, 0, 0.8)"
+                    keyboardType="numeric"
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                  {errors.employee_number && <Text style={tw`text-red-500 mb-4`}>{errors.employee_number.message}</Text>}
+                </>
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: 'La contraseña es obligatoria.' }}
+              render={({ field: { onChange, value } }) => (
+                <View style={tw`relative w-full mb-4`}>
+                  <TextInput
+                    placeholder="Contraseña"
+                    style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black`}
+                    placeholderTextColor="rgba(0, 0, 0, 0.8)"
+                    secureTextEntry={!passwordVisible}
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                  <TouchableOpacity 
+                    style={tw`absolute right-4 top-4`} 
+                    onPress={() => setPasswordVisible(!passwordVisible)}
+                  >
+                    <Ionicons 
+                      name={passwordVisible ? "eye-off" : "eye"} 
+                      size={24} 
+                      color="rgba(0, 0, 0, 0.8)" 
+                    />
+                  </TouchableOpacity>
+                  {errors.password && <Text style={tw`text-red-500 mb-4`}>{errors.password.message}</Text>}
+                </View>
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="confirmar_password"
+              rules={{
+                required: 'Confirma tu contraseña.',
+                validate: (value) => value === control._formValues.password || 'Las contraseñas no coinciden.'
+              }}
+              render={({ field: { onChange, value } }) => (
+                <View style={tw`relative w-full mb-4`}>
+                  <TextInput
+                    placeholder="Confirma tu contraseña"
+                    style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black`}
+                    placeholderTextColor="rgba(0, 0, 0, 0.8)"
+                    secureTextEntry={!confirmPasswordVisible}
+                    value={value}
+                    onChangeText={onChange}
+                  />
+                  <TouchableOpacity 
+                    style={tw`absolute right-4 top-4`} 
+                    onPress={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
+                  >
+                    <Ionicons 
+                      name={confirmPasswordVisible ? "eye-off" : "eye"} 
+                      size={24} 
+                      color="rgba(0, 0, 0, 0.8)" 
+                    />
+                  </TouchableOpacity>
+                  {errors.confirmar_password && <Text style={tw`text-red-500 mb-4`}>{errors.confirmar_password.message}</Text>}
+                </View>
+              )}
+            />
+
+            <TouchableOpacity 
+              style={tw`bg-[#003366] p-4 rounded-lg items-center mt-8 mb-10`}
+              onPress={handleSubmit(handleRegister)} 
+            >
+              <Text style={tw`text-white font-bold text-lg`}>Ingresar</Text>
+            </TouchableOpacity>
           </View>
-          <Text style={tw`text-lg text-gray-500 mt-2`}>Introduce los datos solicitados</Text>
-          <View style={tw`h-0.5 bg-black mt-3 mb-5`} />
-        </View>
-
-        {/* Form Fields */}
-        <TextInput 
-          placeholder="Ingresa tu nombre completo"
-          style={tw`w-full p-4 border border-gray-700 rounded-lg mb-3 text-base bg-transparent text-black`}
-          placeholderTextColor="rgba(0, 0, 0, 0.8)"
-          onChangeText={(value) => handleInputChange('name', value)}
-        />
-
-        <TextInput 
-          placeholder="Ingresa tu correo electrónico"
-          style={tw`w-full p-4 border border-gray-700 rounded-lg mb-3 text-base bg-transparent text-black`}
-          placeholderTextColor="rgba(0, 0, 0, 0.8)"
-          keyboardType="email-address"
-          onChangeText={(value) => handleInputChange('email', value)}
-        />
-
-        <TouchableOpacity onPress={() => setCategoryModalVisible(true)} style={tw`w-full p-4 border border-gray-700 rounded-lg mb-3 flex-row justify-between items-center`}>
-          <Text style={tw`${form.category ? "text-black" : "text-gray-500"}`}>
-            {form.category || "Selecciona tu categoría"}
-          </Text>
-          <Ionicons name="chevron-down" size={24} color="rgba(0, 0, 0, 0.8)" />
-        </TouchableOpacity>
-
-        <TextInput 
-          placeholder="Ingresa tu número de empleado"
-          style={tw`w-full p-4 border border-gray-700 rounded-lg mb-3 text-base bg-transparent text-black`}
-          placeholderTextColor="rgba(0, 0, 0, 0.8)"
-          keyboardType="numeric"
-          onChangeText={(value) => handleInputChange('employeeNumber', value)}
-        />
-
-        <View style={tw`relative w-full mb-3`}>
-          <TextInput 
-            placeholder="Contraseña"
-            style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black`}
-            placeholderTextColor="rgba(0, 0, 0, 0.8)"
-            secureTextEntry={!passwordVisible}
-            onChangeText={(value) => handleInputChange('password', value)}
-          />
-          <TouchableOpacity 
-            style={tw`absolute right-4 top-4`} 
-            onPress={() => setPasswordVisible(!passwordVisible)}
-          >
-            <Ionicons 
-              name={passwordVisible ? "eye-off" : "eye"} 
-              size={24} 
-              color="rgba(0, 0, 0, 0.8)" 
-            />
-          </TouchableOpacity>
-        </View>
-
-        <View style={tw`relative w-full mb-5`}>
-          <TextInput 
-            placeholder="Confirma tu contraseña"
-            style={tw`w-full p-4 border border-gray-700 rounded-lg text-base bg-transparent text-black`}
-            placeholderTextColor="rgba(0, 0, 0, 0.8)"
-            secureTextEntry={!confirmPasswordVisible}
-            onChangeText={(value) => handleInputChange('confirmPassword', value)}
-          />
-          <TouchableOpacity 
-            style={tw`absolute right-4 top-4`} 
-            onPress={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
-          >
-            <Ionicons 
-              name={confirmPasswordVisible ? "eye-off" : "eye"} 
-              size={24} 
-              color="rgba(0, 0, 0, 0.8)" 
-            />
-          </TouchableOpacity>
-        </View>
-
-        <TouchableOpacity 
-          style={tw`bg-[#003366] p-4 rounded-lg items-center mt-8 mb-10`}
-          onPress={handleRegister}
-        >
-          <Text style={tw`text-white font-bold text-lg`}>Ingresar</Text>
-        </TouchableOpacity>
+        </KeyboardAwareScrollView>
 
         {/* Modal */}
         <Modal
@@ -169,9 +274,7 @@ const RegisterScreen = () => {
             </View>
           </View>
         </Modal>
-
-      </View>
-    </ImageBackground>
+      </ImageBackground>
   );
 };
 
