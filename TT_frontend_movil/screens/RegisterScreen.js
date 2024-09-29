4b9cb724d2c42e4b1f134b2217fef9a524bb4cb9
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-09-28 19:12:33 -0600

    Add loadingScreen and redirect register to Login screen

--- TT_frontend_movil/screens/RegisterScreen.js
+++ TT_frontend_movil/screens/RegisterScreen.js
@@ -5,6 +5,8 @@ import tw from 'twrnc';
 import { useForm, Controller } from "react-hook-form";
 import { createAccount } from "../api/accounts.api"; // Import the API
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { useNavigation } from '@react-navigation/native';
+import LoadingScreen from './LoadingScreen'; // Import the LoadingScreen component
 
 const { width, height } = Dimensions.get('window');
 
@@ -18,11 +20,18 @@ const renderErrorMessage = (error) => {
   );
 };
 
-const RegisterScreen = () => {
+const RegisterScreen = (route) => {
+  // -_-_-_-_-_ navigation -_-_-_-_-_
+  const navigation = useNavigation();
+
+  // -_-_-_-_-_ States -_-_-_-_-_
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
   const [categoryModalVisible, setCategoryModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false); // Loading state
+  const [loadingMessage, setLoadingMessage] = useState(""); // Message for LoadingScreen
 
+  // -_-_-_-_-_ Form -_-_-_-_-_
   const { control, handleSubmit, setValue, formState: { errors } } = useForm({
     defaultValues: {
       name: '',
@@ -34,6 +43,7 @@ const RegisterScreen = () => {
     }
   });
 
+  // -_-_-_-_-_ Categories -_-_-_-_-_  
   const categories = [
     "Técnico Docente de Asignatura A",
     "Técnico Docente de Asignatura B",
@@ -55,43 +65,63 @@ const RegisterScreen = () => {
     "Profesor Titular B"
   ];
 
+  // -_-_-_-_-_ Handle Register -_-_-_-_-_
   const handleRegister = async (data) => {
+    setLoading(true);  // Set loading to true when request starts
+
     const password = data.password;
     const confirmPassword = data.confirmar_password;
-  
+
     if (password !== confirmPassword) {
       Alert.alert("Error", "Las contraseñas no coinciden.");
+      setLoading(false);  // Stop loading if passwords don't match
       return;
     }
-  
+
     const { confirmar_password, ...accountData } = data;
-  
+
     try {
+      setLoadingMessage("Creando cuenta...");
       const response = await createAccount(accountData);
-  
-      // Assuming the API returns a successful response
-      if (response.ok) {
+
+      if (response.status === 200 || response.status === 201) {
         Alert.alert("Éxito", "Cuenta creada con éxito");
+        navigation.navigate("Login");
       } else {
-        throw new Error("Unexpected error"); // Fallback for non-400 errors
+        console.log("Response data:", response.data);
+        throw new Error("Unexpected error");
       }
     } catch (error) {
-      // Handle 400 errors explicitly
-      if (error.response && error.response.status === 400) {
-        Alert.alert("Error", "Hubo un error en la solicitud. Verifica los datos ingresados.");
+      console.error("Error response:", error);
+
+      if (error.response) {
+        const apiErrors = error.response.data || {};
+        const email_Error_str = apiErrors.email ? apiErrors.email.join(', ') : '';
+
+        if (error.response.status === 400) {
+          if (apiErrors.email) {
+            Alert.alert("Error", email_Error_str || "Hubo un error en la solicitud.");
+          } else {
+            Alert.alert("Error", "Hubo un error en la solicitud. Verifica los datos ingresados.");
+          }
+        } else {
+          Alert.alert("Error", "Hubo un error al crear la cuenta.");
+        }
       } else {
-        // Handle other errors
-        Alert.alert("Error", "Hubo un error al crear la cuenta.");
+        Alert.alert("Error", "No se pudo conectar con el servidor. Inténtalo más tarde.");
       }
+    } finally {
+      setLoading(false);  // Stop loading once the request completes
     }
   };
 
+  // -_-_-_-_-_ Select Category -_-_-_-_-_
   const selectCategory = (category) => {
-    setValue('category', category, { shouldValidate: true });  // Sets value and triggers validation
+    setValue('category', category, { shouldValidate: true });
     setCategoryModalVisible(false);
   };
-  
 
+  // -_-_-_-_-_ Get Error Message -_-_-_-_-_
   const getErrorMessage = (field) => {
     switch (field) {
       case 'name':
@@ -111,10 +141,14 @@ const RegisterScreen = () => {
     }
   };
 
+  // -_-_-_-_-_ Input Style -_-_-_-_-_
   const inputStyle = (hasError) => tw`w-full p-4 border ${hasError ? 'border-red-500' : 'border-gray-700'} rounded-lg text-base bg-transparent text-black mb-4`;
 
   return (
     <ImageBackground source={require('../assets/images/fondologin.jpg')} style={{ width: width, height: height, flex: 1 }} resizeMode="cover">
+      {/* Show LoadingScreen with a dynamic message */}
+      {loading && <LoadingScreen message={loadingMessage} />}
+
       <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
         <View style={tw`px-5`}>
           <View style={tw`pt-12`}>
@@ -123,6 +157,7 @@ const RegisterScreen = () => {
             <View style={tw`h-0.5 bg-black mt-3 mb-5`} />
           </View>
 
+          {/* Form inputs */}
           <Controller
             control={control}
             name="name"
@@ -135,7 +170,7 @@ const RegisterScreen = () => {
                   value={value}
                   onChangeText={onChange}
                 />
-              {errors.name && renderErrorMessage(getErrorMessage('name'))}
+                {errors.name && renderErrorMessage(getErrorMessage('name'))}
               </>
             )}
           />
@@ -164,6 +199,7 @@ const RegisterScreen = () => {
             )}
           />
 
+          {/* Category selection */}
           <TouchableOpacity onPress={() => setCategoryModalVisible(true)} style={inputStyle(errors.category)}>
             <Controller
               control={control}
@@ -181,6 +217,7 @@ const RegisterScreen = () => {
           </TouchableOpacity>
           {errors.category && renderErrorMessage(getErrorMessage('category'))}
 
+          {/* Employee number input */}
           <Controller
             control={control}
             name="employee_number"
@@ -199,6 +236,7 @@ const RegisterScreen = () => {
             )}
           />
 
+          {/* Password and Confirm Password Inputs */}
           <Controller
             control={control}
             name="password"
@@ -212,14 +250,14 @@ const RegisterScreen = () => {
                   value={value}
                   onChangeText={onChange}
                 />
-                <TouchableOpacity 
-                  style={tw`absolute right-4 top-4`} 
+                <TouchableOpacity
+                  style={tw`absolute right-4 top-4`}
                   onPress={() => setPasswordVisible(!passwordVisible)}
                 >
-                  <Ionicons 
-                    name={passwordVisible ? "eye-off" : "eye"} 
-                    size={24} 
-                    color="rgba(0, 0, 0, 0.8)" 
+                  <Ionicons
+                    name={passwordVisible ? "eye-off" : "eye"}
+                    size={24}
+                    color="rgba(0, 0, 0, 0.8)"
                   />
                 </TouchableOpacity>
                 {errors.password && renderErrorMessage(getErrorMessage('password'))}
@@ -243,14 +281,14 @@ const RegisterScreen = () => {
                   value={value}
                   onChangeText={onChange}
                 />
-                <TouchableOpacity 
-                  style={tw`absolute right-4 top-4`} 
+                <TouchableOpacity
+                  style={tw`absolute right-4 top-4`}
                   onPress={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
                 >
-                  <Ionicons 
-                    name={confirmPasswordVisible ? "eye-off" : "eye"} 
-                    size={24} 
-                    color="rgba(0, 0, 0, 0.8)" 
+                  <Ionicons
+                    name={confirmPasswordVisible ? "eye-off" : "eye"}
+                    size={24}
+                    color="rgba(0, 0, 0, 0.8)"
                   />
                 </TouchableOpacity>
                 {errors.confirmar_password && renderErrorMessage(getErrorMessage('confirmar_password'))}
@@ -258,7 +296,12 @@ const RegisterScreen = () => {
             )}
           />
 
-          <TouchableOpacity style={tw`bg-[#003366] p-4 rounded-lg items-center mt-8 mb-10`} onPress={handleSubmit(handleRegister)}>
+          {/* Submit Button */}
+          <TouchableOpacity
+            style={tw`bg-[#003366] p-4 rounded-lg items-center mt-8 mb-10`}
+            onPress={handleSubmit(handleRegister)}
+            disabled={loading} // Disable button during loading
+          >
             <Text style={tw`text-white font-bold text-lg`}>Ingresar</Text>
           </TouchableOpacity>
         </View>
@@ -266,30 +309,28 @@ const RegisterScreen = () => {
 
       {/* Modal de categorías */}
       <Modal visible={categoryModalVisible} transparent={true} animationType="slide">
-  <View style={tw`flex-1 justify-center items-center bg-[rgba(0,0,0,0.5)]`}>
-    {/* Smaller and more stylized modal */}
-    <View style={tw`w-4/5 h-1/2 bg-white rounded-lg p-5`}>
-      {/* ScrollView for scrollable content */}
-      <ScrollView>
-        {categories.map((item) => (
-          <TouchableOpacity 
-            key={item} 
-            style={tw`py-3 border-b border-gray-300`} 
-            onPress={() => selectCategory(item)}
-          >
-            <Text>{item}</Text>
-          </TouchableOpacity>
-        ))}
-      </ScrollView>
-      <TouchableOpacity 
-        style={tw`mt-5 items-center`} 
-        onPress={() => setCategoryModalVisible(false)}
-      >
-        <Text style={tw`text-blue-700 text-base`}>Cerrar</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-</Modal>
+        <View style={tw`flex-1 justify-center items-center bg-[rgba(0,0,0,0.5)]`}>
+          <View style={tw`w-4/5 h-1/2 bg-white rounded-lg p-5`}>
+            <ScrollView>
+              {categories.map((item) => (
+                <TouchableOpacity
+                  key={item}
+                  style={tw`py-3 border-b border-gray-300`}
+                  onPress={() => selectCategory(item)}
+                >
+                  <Text>{item}</Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+            <TouchableOpacity
+              style={tw`mt-5 items-center`}
+              onPress={() => setCategoryModalVisible(false)}
+            >
+              <Text style={tw`text-blue-700 text-base`}>Cerrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </ImageBackground>
   );
 };
