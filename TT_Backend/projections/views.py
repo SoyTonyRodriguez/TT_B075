da13857acde88dd64afb0584e02fb971c3084534
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-10-02 20:27:28 -0600

    Feat: Create projections app and logic to works together tasks app

---
+++ TT_Backend/projections/views.py
@@ -0,0 +1,63 @@
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
+
+from .serializer import RegisterProjectionSerializer, ProjectionSerializer
+from .models import Projection
+
+# Create your views here.
+class RegisterProjectionView(APIView):
+    parser_classes = [JSONParser, MultiPartParser, FormParser]
+    permission_classes = (IsAuthenticated,)
+    queryset = Projection.objects.all()
+    serializer_class = RegisterProjectionSerializer
+
+    def post(self, request):
+        print(request.data)
+        serializer = RegisterProjectionSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+class GetProjectionsView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProjectionSerializer
+
+    def get_queryset(self):
+        account_id = self.kwargs['account_id']
+        return Projection.objects.filter(account_id=account_id)
+
+class EditProjectionView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = RegisterProjectionSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        # Restrict the queryset to only the projections that belong to the authenticated user's account
+        return Projection.objects.filter(account_id=self.request.user.id)
+    
+    def perform_update(self, serializer):
+        # Ensure that the projection belongs to the authenticated user's account before updating
+        projection = self.get_object()
+        if projection.account_id != self.request.user.id:
+            raise PermissionDenied("You do not have permission to edit this projection.")
+        serializer.save()
+
+class DeleteProjectionView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = RegisterProjectionSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        # Only return projections that belong to the authenticated user
+        return Projection.objects.filter(account_id=self.request.user.id)
+    
+    def perform_destroy(self, instance):
+        # Check if the projection belongs to the authenticated user
+        if instance.account_id != self.request.user.id:
+            raise PermissionDenied("You do not have permission to delete this projection.")
+        instance.delete()
