6a86753d65d5f247012b1302025b46f719948e42
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-11-17 16:53:35 -0600

    Update: adming django spanish

--- TT_Backend/default_Projection_Dates/apps.py
+++ TT_Backend/default_Projection_Dates/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class DefaultProjectionDatesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'default_Projection_Dates'
+    verbose_name = _('Fechas de Proyección por Defecto')
--- TT_Backend/documents_admin/apps.py
+++ TT_Backend/documents_admin/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class DocumentsAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'documents_admin'
+    verbose_name = _('Documentos oficiales')  # Traducción al español
