ac13509ea51a15d453b8b201d86dcdbc31e00481
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-11-18 13:48:43 -0600

    Update: trying see css in prod

--- TT_Backend/django_API_TT/settings.py
+++ TT_Backend/django_API_TT/settings.py
@@ -209,7 +209,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Directorio donde se almacenarán
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Asegúrate de que tenga el slash inicial y final
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
