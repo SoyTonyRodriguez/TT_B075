7216875edb3ff262ec5ce398cdc692238d48c7e5
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-11-17 18:37:17 -0600

    update urls

--- TT_frontend_web/vite.config.js
+++ TT_frontend_web/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["axios"], // Esto asegura que axios no cause problemas en la construcción
-    },
-  },
 })
