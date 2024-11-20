3d20cbed94daf8323223c7339340729dd6ad4c33
Antonio Rodriguez <rodriguez.flores.antonioo@gmail.com>
2024-11-19 18:53:05 -0600

    Fix: change url to api files web

--- TT_frontend_web/src/api/tasks.api.js
+++ TT_frontend_web/src/api/tasks.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Initial set-up
 const TasksAPI = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
 // create task method
