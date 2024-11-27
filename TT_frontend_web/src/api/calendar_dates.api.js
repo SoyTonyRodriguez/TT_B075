914053a7f960ab5ed8dade3508859ef40cef290e
Luis Casaos <casaos022@gmail.com>
2024-11-26 22:11:35 -0600

    Guide Screen added

--- TT_frontend_web/src/api/calendar_dates.api.js
+++ TT_frontend_web/src/api/calendar_dates.api.js
@@ -2,20 +2,9 @@ import axios from "axios";
 
 // Initial set-up
 const accountsAPI = axios.create({
-    //baseURL: import.meta.env.VITE_API_BASE_URL,
-    baseURL: 'http://localhost:8000/api/v1',
-})
-
-// getall dates
-export const getAllDates = () => accountsAPI.get(`calendar_date/`);
-
-import axios from "axios";
-
-// Initial set-up
-const accountsAPI = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
+    //baseURL: 'http://localhost:8000/api/v1',
 })
 
 // getall dates
 export const getAllDates = () => accountsAPI.get(`calendar_date/`);
-
